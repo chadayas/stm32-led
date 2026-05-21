f1fa70b268beb4073e37f032dc8b3bb4b97dae39
chadayas <cgassoya@gmail.com>
2026-05-20 23:17:44 -0400

    trying out blink example

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,32 @@
+extern "C" {
+#include <stm32f4xx_hal_gpio.h>
+}
 
-int main(){
+// Nucleo F446RE - LD2 is on PA5
+#define LED_PIN  GPIO_PIN_5
+#define LED_PORT GPIOA
 
+static void led_init(void)
+{
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+
+    GPIO_InitTypeDef gpio = {};
+    gpio.Pin   = LED_PIN;
+    gpio.Mode  = GPIO_MODE_OUTPUT_PP;
+    gpio.Pull  = GPIO_NOPULL;
+    gpio.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(LED_PORT, &gpio);
+}
+
+int main(void)
+{
+    HAL_Init();
+    led_init();
+
+    while (1) {
+        HAL_GPIO_TogglePin(LED_PORT, LED_PIN);
+        HAL_Delay(500);
+    }
+
+    return 0;
 }
