ae22b3616dc8916696a9b01685c8419f901cd390
chadayas <cgassoya@gmail.com>
2026-05-21 15:58:49 -0400

    adding device tree overlay

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,17 @@
 #include <zephyr/kernel.h>
-#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/led_strip.h>
+#include <zephyr/dt-bindings/led/led.h>
 
 #define LED_NODE DT_ALIAS(led0)
-
+void led_strip_init(){
+   const struct device* dev = DEVICE_DT_GET(DT_ALIAS(led_strip)); 
+   struct led_rgb pixels[256]{0}; 
+   pixels[0].b = 255; 
+   led_strip_update_rgb(device ,pixels, 256);
+}
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
 
-int main(void)
-{
-    gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-
-    while (1) {
-        gpio_pin_toggle_dt(&led);
-        k_msleep(500);
-    }
-
-    return 0;
+int main(){
+   
+  return 0;
 }
