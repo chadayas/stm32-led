67a880e252ccebb082956d139cd8bb7aced4c0f3
chadayas <cgassoya@gmail.com>
2026-05-22 22:52:00 -0400

    adding logging, adding uart console functionaility in conf

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,19 @@
 #include <zephyr/drivers/led_strip.h>
 #include <zephyr/dt-bindings/led/led.h>
 #include <zephyr/device.h>
+#include <zephyr/logging/log.h>
 
-#define LED_NODE DT_ALIAS(led0)
+LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
+
 void led_strip_init(){
-   const struct device* dev = DEVICE_DT_GET(DT_ALIAS(led_strip)); 
-   struct led_rgb pixels[256]{0}; 
-   pixels[0].b = 255; 
+   const struct device* dev = DEVICE_DT_GET(DT_ALIAS(led_strip));
+   if (!device_is_ready(dev)) {
+      LOG_ERR("LED strip device not ready");
+      return;
+   }
+   LOG_INF("LED strip ready, updating pixels");
+   struct led_rgb pixels[256]{0};
+   pixels[0].b = 255;
    led_strip_update_rgb(dev, pixels, 256);
 }
 
