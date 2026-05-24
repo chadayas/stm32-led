fabdafa7bd4c018b894205edbcf6cd3f83970fa0
chadayas <cgassoya@gmail.com>
2026-05-24 14:26:33 -0400

    testing out led animations, changing timing per the worldsemi datasheet

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,28 @@
-#include <zephyr/kernel.h>
-#include <zephyr/drivers/led_strip.h>
-#include <zephyr/dt-bindings/led/led.h>
-#include <zephyr/device.h>
-#include <zephyr/logging/log.h>
+#include<zephyr/kernel.h>
+#include<zephyr/drivers/led_strip.h>
+#include<zephyr/dt-bindings/led/led.h>
+#include<zephyr/device.h>
+#include<zephyr/logging/log.h>
+#include<cstdint>
+
+constexpr uint16_t MAX {256};
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
+
+void led_animation(const struct device* dev){
+   uint8_t i;
+   static struct led_rgb pixels[MAX];
+   for (i=0; i < MAX-1; i++){
+      pixels[0].b = i; 
+      auto update = led_strip_update_rgb(dev, pixels, MAX);
+      if (update != 0){
+         LOG_ERR("LED strip was not updated");
+         return;  
+      } else
+      LOG_INF("update was fine"); 
+   } 
+}
+
 
 void led_strip_init(){
    const struct device* dev = DEVICE_DT_GET(DT_ALIAS(led_strip));
@@ -13,15 +31,7 @@ void led_strip_init(){
       return;
    }
    LOG_INF("LED strip ready");
-   static struct led_rgb pixels[256]{0};
-   pixels[0].b = 255;
-   
-   auto update = led_strip_update_rgb(dev, pixels, 256);
-   if (update != 0){
-      LOG_ERR("LED strip was not updated");
-      return;  
-   } else
-      LOG_INF("update was fine");
+   led_animation(dev);    
 }
 
 
